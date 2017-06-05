1c6bb62c365b378241d201fd101579e306a7877b
fajrulfala <fajrul362@gmail.com>
2017-06-05 10:12:12 +0700

    Add files via upload

---
+++ Latihan7.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<form action="biodatapost.php" method="post">
+<pre>
+Nama		: <input type="text" name="nama"  />
+
+Alamat		: <input type="text" name="alamat"  />
+
+Email		: <input type="text" name="email"  />
+
+Komentar	: <input type="text" name="komentar"  />
+
+		<input type="submit"/ name="kirim" value="Kirim">
+</pre>
+</form>
+</body>
+</html>
